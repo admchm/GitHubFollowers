705030bc13f22f92279894d2c6ffbd40a309162f
Adam Chomicki <admchm@outlook.com>
2023-03-16 08:27:25 +0100

    Initialize project

---
+++ GitHubFollowers/GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Adam Chomicki on 15/03/2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tintColor
+    }
+}
---
+++ GitHubFollowers/GitHubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Adam Chomicki on 15/03/2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+}
